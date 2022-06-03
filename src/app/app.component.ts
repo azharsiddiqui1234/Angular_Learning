3c979e04485b4f8cc8e92294ab640fa58c665c78
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-03 19:58:41 +0530

    Header Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog'; // Application Name
-  color = 'red';
-  bgColor = "cyan"
 
-  changeColor() {
-    let newColor = ['green', 'blue', 'red', 'yellow', 'red'];
-    let randomNumber = Math.random() * newColor.length;
-    let randomColor = newColor[Math.floor(randomNumber)];
-    this.color = randomColor;
-
-
-    
-  }
+  
 }
