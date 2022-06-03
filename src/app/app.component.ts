c5e550ad05a33f8f142c51355a68242e5da2b4c2
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-03 14:46:07 +0530

    simple_loop_concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'blog'; // Application Name
-  color= 'xyz'
+  user = [
+    {
+      Name: 'Azhar Aminoddin Sayyed',
+      Age: 20,
+      ContactNo: 9088997998,
+    },
+    {
+      Name: 'Shoaib Hasham Khatri',
+      Age: 20,
+      ContactNo: 8977677887,
+    },
+    {
+      Name: 'Azhar Aminoddin Sayyed',
+      Age: 20,
+      ContactNo: 7538569797,
+    },
+  ];
 }
